a9afaa646eaee91e081d1a3242a15d242b1cd7cf
Marcello <marcelloangileri@gmail.com>
2015-08-04 23:31:34 +0200

    Initial Commit

---
+++ sites/all/modules/mailfish/templates/mailfish-block.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @File
+ * Theme the mailfish block
+ *
+ *
+ * Available vars:
+ * $form: the rendered form
+ */
+?>
+
+<div id="mailfish-rocks">
+    <?php print t('Check it out!'); ?>
+    <?php print $form; ?>
+</div>
